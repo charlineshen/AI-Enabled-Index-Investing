e0e9215c8ecf07d861a3f950ebf1112333df5188
Jingwen Zhang <jingwen_zhang@g.harvard.edu>
2024-09-27 21:14:05 -0400

    initial commit of rag pipeline

---
+++ data-process.py
@@ -0,0 +1,34 @@
+import PyPDF2
+import os
+
+# Function to extract text from the PDF
+def extract_text_from_pdf(pdf_path):
+    text = ""
+    with open(pdf_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        for page in range(len(reader.pages)):
+            text += reader.pages[page].extract_text()
+    return text
+
+def save_text_to_file(text, pdf_path):
+    # Extract the filename without extension
+    text_dir = os.path.dirname(pdf_path).replace('index-doc', 'index-txt')
+    print('text_dir: ' + text_dir)
+    base_name = os.path.basename(pdf_path).replace('.pdf', '.txt')
+    print('txt_path: ' + base_name)
+    # Construct the new path for the .txt file
+    txt_path = os.path.join(text_dir, base_name)
+    
+    # Write the extracted text to the file
+    with open(txt_path, 'w', encoding='utf-8') as text_file:
+        text_file.write(text)
+    
+    return txt_path
+
+if __name__ == '__main__':
+    pdf_path = "/Users/jingwen/Desktop/2024 Fall/AC 297/AI-enabled-Index-Investing/index-doc/MSCI_Select_ESG_Screened_Indexes_Methodology_20230519.pdf"
+    text = extract_text_from_pdf(pdf_path)
+    
+    # Save the text to a file in the specified folder
+    saved_file_path = save_text_to_file(text, pdf_path)
+    print(f"Text saved to: {saved_file_path}")
