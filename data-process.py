940d9e6e6bc680b51685d195a58f21570317bf74
Jingwen Zhang <jingwen_zhang@g.harvard.edu>
2024-09-29 16:04:17 -0400

    update data preprocessing output location

--- data-process.py
+++ data-process.py
@@ -13,12 +13,12 @@ def extract_text_from_pdf(pdf_path):
 
 def save_text_to_file(text, pdf_path):
     # Extract the filename without extension
-    text_dir = os.path.dirname(pdf_path).replace('index-doc', 'index-txt')
-    print('text_dir: ' + text_dir)
+    text_dir = os.path.dirname(pdf_path).replace('index-doc', 'input-datasets/books')
     base_name = os.path.basename(pdf_path).replace('.pdf', '.txt')
-    print('txt_path: ' + base_name)
     # Construct the new path for the .txt file
     txt_path = os.path.join(text_dir, base_name)
+    print('txt_path: ' + txt_path)
+
     
     # Write the extracted text to the file
     with open(txt_path, 'w', encoding='utf-8') as text_file:
@@ -29,6 +29,6 @@ def save_text_to_file(text, pdf_path):
 if __name__ == '__main__':
     pdf_path = "/Users/jingwen/Desktop/2024 Fall/AC 297/AI-enabled-Index-Investing/index-doc/MSCI_Select_ESG_Screened_Indexes_Methodology_20230519.pdf"
     text = extract_text_from_pdf(pdf_path)
-    unidecode.unidecode(text)
-    saved_file_path = save_text_to_file(text, pdf_path)
+    ascii_text = unidecode.unidecode(text)
+    saved_file_path = save_text_to_file(ascii_text, pdf_path)
     print(f"Text saved to: {saved_file_path}")
